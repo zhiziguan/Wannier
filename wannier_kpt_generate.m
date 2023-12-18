8a4716c359a1375f92d7e24e002b762a7cff9b43
zhiziguan <138292646+zhiziguan@users.noreply.github.com>
2023-12-18 17:33:11 +0800

    Update wannier_kpt_generate.m

--- wannier_kpt_generate.m
+++ wannier_kpt_generate.m
@@ -6,10 +6,12 @@ outputFileID = fopen('wannier90_geninterp.kpt', 'w');
 
 numRows = size(data, 1);
 
+fprintf(outputFileID, '#Comment \n');
+fprintf(outputFileID, 'frac \n');
 for i = 1:numRows
-    fprintf(outputFileID, '#Comment \n');
-    fprintf(outputFileID, 'frac \n');
     fprintf(outputFileID, '%d %f %f %f\n', i, data(i, :));
 end
 
 fclose(outputFileID);
+
+
